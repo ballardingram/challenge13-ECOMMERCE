59c91d06b27a9389252cd64c7d0dddf1523ac9ec
Ballard Ingram <ballardingram@outlook.com>
2022-05-17 12:52:34 -0500

    refactored - removed unnecessary allow null

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,17 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = process.env.JAWSDB_URL
-? new Sequelize(process.env.JAWSDB_URL)
-: new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  host: 'localhost',
-  dialect: 'mysql',
-  dialectOptions: {
-    decimalNumbers: true,
-  },
-});
+let sequelize;
+if(process.env.JAWSB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql'
+    });
+}
 
 module.exports = sequelize;
--- models/index.js
+++ models/index.js
@@ -18,12 +18,14 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
+  as: 'productTag_Tag',
   foreignKey: 'product_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
+  as: 'productTag_Tag',
   foreignKey: 'tag_id'
 })
 
