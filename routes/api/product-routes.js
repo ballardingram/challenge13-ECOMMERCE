d0aaf58d94141349481c4462a3fe1580157836d7
Ballard Ingram <ballardingram@outlook.com>
2022-05-17 09:08:05 -0500

    fixed error with vs code adding in lib route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { UPSERT } = require('sequelize/types/lib/query-types');
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
