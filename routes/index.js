799bd8ee2f59aaf2cdda772d85b234a553b4ddb9
Ballard Ingram <ballardingram@outlook.com>
2022-05-17 08:43:36 -0500

    routes/index - reverted back to starter code

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,10 @@
 const router = require('express').Router();
-const { Product } = require('../models');
 const apiRoutes = require('./api');
-const CategoryRoutes = require ('./api/category-routes');
-const ProductRoutes = require ('./api/product-routes');
-const TagRoutes = require ('./api/tag-routes');
 
 router.use('/api', apiRoutes);
-router.use('/api', CategoryRoutes);
-router.use('/api', ProductRoutes);
-router.use('/api', TagRoutes);
 
 router.use((req, res) => {
-  res.status(404).end();
+  res.send("<h1>Wrong Route!</h1>")
 });
 
 module.exports = router;
